bc46cf8ea3303912486a40ee533e51aed65aa284
ugin <ugin@cognitivebot.io>
2019-09-02 15:38:47 +0300

    stable

--- temp.js
+++ temp.js
@@ -4,50 +4,17 @@
 
 const util = require('util');
 const TObject = require('./xrsu/TObject');
+const {User} = require('./temp1');
 
 //-------------------------------------------------------------------------------------------------
 
+let oj = {
+    aa: {cc: 1},
+    bb: 2
+};
 
-var Grandfather = function () {}; // Конструктор Grandfather
-Grandfather.prototype.color = 'green';
+let {aa: {cc}} = oj;
 
-var Father = function () {}; // Конструктор Father
-Father.prototype = new Grandfather(); // Это простой, но не самый лучший вариант протитипного наследования
+console.log('cc [' + cc + ']');
 
-var Son = function () {}; // Конструктор Son
-Son.prototype = new Father(); // Аналогично
-
-var u = new Grandfather(); // Экземпляр "класса" Grandfather
-var f = new Father(); // Экземпляр "класса" Father
-var s = new Son(); // Экземпляр "класса" Son
-
-
-// Изначально все зеленые
-console.log([u.color, f.color, s.color]); // ["green", "green", "green"]
-
-// Дед решил поменять свой цвет и цвет потомства
-Grandfather.prototype.color = 'blue';
-console.log([u.color, f.color, s.color]); // ["blue", "blue", "blue"]
-
-// Отец решил все вернуть для себя и своего потомства
-Father.prototype.color = 'green';
-// Хотя мог исделать и так:
-// Grandfather.prototype.color = 'green';
-console.log([u.color, f.color, s.color]); // ["blue", "green", "green"]
-TObject.infoConsole_B('[', f, ']');
-
-// Смысла нет
-Grandfather.prototype.color = 'blue';
-console.log([u.color, f.color, s.color]); // ["blue", "green", "green"]
-
-// Сын решил не брать пример с Деда и поменял только собственное свойство
-s.color = 'black'; // Меняем собственное свойство, которое не затрагивает цепочку прототипов
-console.log([u.color, f.color, s.color]); // ["blue", "green", "black"]
-
-var SonsSon = function () {}; // Конструктор SonsSon
-SonsSon.prototype = new Son(); // Аналогично
-
-var ss = new SonsSon(); // Экземпляр "класса" SonsSon
-// Сын сына унаследовал от Отца
-console.log([u.color, f.color, s.color, ss.color]); // ["blue", "green", "black", "green"]
 
--- temp1.js
+++ temp1.js
@@ -7,13 +7,3 @@ const TObject = require('./xrsu/TObject');
 
 //-------------------------------------------------------------------------------------------------
 
-function User(name) {
-    this.name = name;
-}
-
-User.prototype.age = 22;
-
-let pete = new User('Pete');
-let mary = new User('Mary');
-
-TObject.infoConsole_B('', pete, '');
